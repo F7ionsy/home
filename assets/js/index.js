050be240580809fe5f8251896fddbc820ec69e1c
fakeYanss <yanshisangc@gmail.com>
2019-03-23 13:40:33 +0800

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,3 +12,17 @@ $(document).ready(function(){
   console.log('特别鸣谢zchen9小仙女的精彩主题，希望大家去逛逛她的github：https://github.com/zchen9');
   
 });
+
+<script text="module">
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(function() {
+      })
+      .catch(function(error) {
+        console.log("Service worker registration failed:", error);
+      });
+  } else {
+    console.log("Service workers are not supported.");
+  }
+</script>
